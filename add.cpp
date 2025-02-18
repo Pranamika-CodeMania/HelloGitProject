2869d50325b05a80b85b20e4fd8f4d721629a428
Pranamika Paul <pranamikapaul24@gmail.com>
2025-02-18 21:15:36 +0400

    Addition of 2 nos

--- add.cpp
+++ add.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     cout << "Addition!" << std::endl;
     int a,b,c;
-    a=10;
-    b=20;
+    a=80;
+    b=90;
     c=a+b;
 
     cout<<"Sum is :" << c << endl;
